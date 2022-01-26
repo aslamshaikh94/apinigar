8e035354cbccae1d7e8bc0cbe1479a26e1688f0d
Aslam Ahmed <highflyingaslam@gmail.com>
2022-01-26 23:46:57 +0530

    changes

--- server.js
+++
@@ -1,5 +0,0 @@
-// server.js
-// @see https://strapi.io/documentation/v3.x/getting-started/deployment.html#application-configuration
-const strapi = require("strapi");
-
-strapi(/* {...} */).start();
